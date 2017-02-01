5cd5e4e6c5bfbc3726483538bc4e874ce0802dd9
spschoen <spschoen@ncsu.edu>
2017-02-01 18:19:03 -0500

    Printing CLOC to file.

--- CLOC.py
+++ CLOC.py
@@ -20,7 +20,9 @@ import subprocess
     #sys.exit()
 
 #Commented out because doesn't work on Windows.
-#subprocess.call(["cloc","./", "--by-file-by-lang", "--exclude-ext=xml", "--exclude-dir=gui,reference,output", "--xml", "--out=cloc.xml"])
+#subprocess.call(["cloc","./", "--by-file-by-lang", \
+# "--exclude-ext=xml", "--exclude-dir=gui,reference,output", \
+# "--xml", "--out=cloc.xml"])
 
 #Get the parser, set it up to parse cloc.xml
 try:
@@ -30,18 +32,14 @@ except:
     print("Script exiting.")
     sys.exit()
 
-#root is the first node in the XML file.
-root = DOMTree.documentElement
 
 #Get the elements named 'language' and save the first one as root.
-#We do this since checkstyle XML output has a node that wraps a single
-#node containing all the errors, so there's no reason to look at that
-#original root beyond making sure the version is correct.
-root = root.getElementsByTagName('languages')[0]
+#We do this since CLOC's XML output has a node that wraps a single
+#node containing all the languages, so there's no reason to look at that
+#original root
+root = DOMTree.documentElement.getElementsByTagName('languages')[0]
 
-#A basic for loop, to look at all the nodes (nodes) inside the file node
-#(which is now the root node) and print out their information.
-#.childNodes is a list of nodes that the root has as children.  I hate E 115.
+output = ""
 for node in root.childNodes:
     #Nodes have like, 11 types, in Python, and this is a special one that's
     #Called text.  I'm not sure what the TEXT_NODE is but every single
@@ -49,11 +47,42 @@ for node in root.childNodes:
     if node.nodeType == node.TEXT_NODE:
         continue;
     if node.hasAttribute("name") and "Java" in node.getAttribute("name"):
-        if node.hasAttribute("files_count"):
-            print("Files  : " + node.getAttribute("files_count"))
-        if node.hasAttribute("blank"):
-            print("Blank  : " + node.getAttribute("blank"))
-        if node.hasAttribute("comment"):
-            print("Comment: " + node.getAttribute("comment"))
-        if node.hasAttribute("code"):
-            print("Code   : " + node.getAttribute("code"))
+        if node.hasAttribute("files_count") and \
+            not node.getAttribute("files_count") == "":
+            #print("Files  : " + node.getAttribute("files_count"))
+            output += node.getAttribute("files_count") + " "
+        else:
+            print("ERROR: No file count attribute")
+            print("Script exiting.")
+            sys.exit()
+
+        if node.hasAttribute("blank") and \
+            not node.getAttribute("blank") == "":
+            #print("Blank  : " + node.getAttribute("blank"))
+            output += node.getAttribute("blank") + " "
+        else:
+            print("ERROR: No blank line attribute")
+            print("Script exiting.")
+            sys.exit()
+
+        if node.hasAttribute("comment") and \
+            not node.getAttribute("comment") == "":
+            #print("Comment: " + node.getAttribute("comment"))
+            output += node.getAttribute("comment") + " "
+        else:
+            print("ERROR: No comment line attribute")
+            print("Script exiting.")
+            sys.exit()
+
+        if node.hasAttribute("code") and not node.getAttribute("code") == "":
+            #print("Code   : " + node.getAttribute("code"))
+            output += node.getAttribute("code")
+        else:
+            print("ERROR: No lines of code attribute")
+            print("Script exiting.")
+            sys.exit()
+
+print(output)
+outFile = open("cloc.stat", "w")
+outFile.write(output)
+outFile.close()
