b38790d4ac2b1d96b968693d5e5275b9c6b8a239
spschoen <spschoen@ncsu.edu>
2017-02-10 14:29:16 -0500

    Fixing findbugs.py

--- Parsers/FindBugs.py
+++ Parsers/FindBugs.py
@@ -5,19 +5,16 @@ import xml.dom.minidom
 import sys
 import os
 
-print(len(sys.argv))
-print(str(sys.argv))
+#print(len(sys.argv))
+#print(str(sys.argv))
 
-EXEC_DIR = os.getcwd()
-'''FILE_DIR = os.path.abspath(os.path.join(EXEC_DIR, \
-    str(sys.argv[len(sys.argv) - 1].split("/")), "findBugs.xml"))
-print(FILE_DIR)'''
-
-FILE_DIR = os.path.abspath(os.path.join(EXEC_DIR))
+FILE_DIR = os.path.abspath(os.path.join(os.getcwd()))
 print(FILE_DIR)
 for arg in sys.argv[len(sys.argv) - 1].split("/"):
+    if arg == "":
+        continue
     FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, arg))
-    print(FILE_DIR)
+    #print(FILE_DIR)
 
 try:
     DOMTree = xml.dom.minidom.parse(FILE_DIR + '/findbugs.xml')
@@ -32,32 +29,30 @@ root = DOMTree.documentElement
 if root.hasAttribute("version"):
     print("FindBugs Version : %s" % root.getAttribute("version"))
 
-for node in root.getElementsByTagName("BugInstance"):
-    if node.nodeType != node.TEXT_NODE:
-        line = "Bugtype: "
+for node in root.childNodes:
+    if node.nodeName == "BugInstance":
+        line = "Bugtype : "
         line += node.getAttribute("type") + "\n"
         if node.hasAttribute("priority"):
-            line += "Priority: " + node.getAttribute("priority")
+            line += "Priority: " + node.getAttribute("priority") + "\n"
         if node.hasAttribute("rank"):
-            line += " Rank: " + node.getAttribute("rank") + "\n"
+            line += "Rank    : " + node.getAttribute("rank") + "\n"
         if node.hasAttribute("abbrev"):
             print()
-            line += "Abbrev: " + node.getAttribute("abbrev") + "\n"
+            line += "Abbrev  : " + node.getAttribute("abbrev") + "\n"
         if node.hasAttribute("category"):
             line += "Category: " + node.getAttribute("category")
 
         print(line)
-
-        classNode = root.getElementsByTagName('Class')[0]
-        if classNode.hasAttribute("classname"):
-            string = classNode.getAttribute("classname").split(".")
-            string = string[len(string) - 1]
-            print("Class: " + string)
+        for classNode in node.childNodes:
+            if classNode.nodeName == "Method":
+                if classNode.hasAttribute("classname"):
+                    string = classNode.getAttribute("classname").split(".")
+                    string = string[len(string) - 1]
+                    print("Class   : " + string)
+                if classNode.hasAttribute("name"):
+                    print("Method  : " + classNode.getAttribute("name"))
 
         #I have no idea what this is looking for.
         methodNode = root.getElementsByTagName('Method')[0]
-        if methodNode.hasAttribute("name"):
-            print(methodNode.getElementsByTagName("name")) #Prints the brackets, look at xml <Method classname> for deets
-
-
-    print()
+         #Prints the brackets, look at xml <Method classname> for deets
