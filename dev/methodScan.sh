6448457f919d3d40d40859c556880f59e5a82d17
spschoen <spschoen@ncsu.edu>
2017-02-14 22:21:40 -0500

    Starting real parsers.
    checkstyleUpload will scan checkstyle xml and upload to MySQL
    classScan will scan the directory supplied for all classes, to add Classes
    to the ClassUID table
    commitUpload will analyze the commit and upload it to the commit Table
    coverageUpload will parse coverage and upload to the coverage Table
    findbugs ... you get the point on this one.
    methodScan is like class Scan, but does scans for methods.
    pmdUpload is, as above.
    Pull_test is going to be deleted next commit.
    testScan will scan the testDirectory for tests specifically.  Probably can
    be wrapped under methodScan and do some CLA funk.

---
+++ dev/methodScan.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function dirScan { 
+    cd "$1"
+    #echo "Dir: $1"
+    for D in *; do
+        if [ -d "${D}" ] && [[ "${D}" != "gui" ]]; then
+            #echo "dir ${D}"
+            dirScan "${D}"
+        else
+            
+            if [[ "${D}" == *.java ]]; then
+                echo "    ${D}"
+                less "${D}" | egrep -o '(publ|priv|prot).*' | grep "(" | egrep -v "\*|;"
+            fi
+        fi
+    done
+    cd ..
+}
+
+#Don't forget to change directory to wherever we are.
+cd "$(dirname "$0")"
+
+#Check if user supplied arguments
+if [ $# -ne 1 ]; then
+    echo "ERR: Not given expected arguments.."
+    echo "Expected [directory of directories interact with]"
+    echo "Exiting program with status 0."
+    exit 0
+fi
+
+if [ ! -d $1 ]; then
+    echo "ERR: Argument 1 is not a directory, or directory does not exist."
+    echo "Exiting program with status 0."
+    exit 0
+fi
+
+#Saving directory.
+DIRECTORY="$1"
+
+dirScan $DIRECTORY
