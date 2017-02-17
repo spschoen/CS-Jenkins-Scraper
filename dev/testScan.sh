5c0a1ba3323f33c5b5ba17b21e05a15221bd899b
spschoen <spschoen@ncsu.edu>
2017-02-17 15:33:04 -0500

    Scanner for Tests.

---
+++ dev/testScan.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function dirScan {
+    cd "$1"
+    #echo "Dir: $1"
+    for D in *; do
+        if [ -d "${D}" ] && [[ "${D}" != "gui" ]] && [[ "${D}" != "src" ]]; then
+            #echo "dir ${D}"
+            dirScan "${D}"
+        else
+            if [[ "${D}" == *.java ]]; then
+                less "${D}" | egrep -o '(publ|priv|prot).*' | egrep -v "\*|;"
+                echo ""
+            fi
+        fi
+    done
+    cd ..
+}
+
+#Don't forget to change directory to wherever we are.
+cd "$(dirname "$0")"
+
+#Check if user supplied arguments
+if [ $# -ne 1 ]; then
+    echo "ERR: Not given expected arguments.."
+    echo "Expected [directory of directories interact with]"
+    echo "Exiting program with status 0."
+    exit 0
+fi
+
+if [ ! -d $1 ]; then
+    echo "ERR: Argument 1 is not a directory, or directory does not exist."
+    echo "Exiting program with status 0."
+    exit 0
+fi
+
+#Saving directory.
+DIRECTORY="$1"
+
+dirScan $DIRECTORY
