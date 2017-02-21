a7f057dffc4a021fc8fbf007509a3de5336d680a
spschoen <spschoen@ncsu.edu>
2017-02-21 18:23:45 -0500

    Test scan works; checks if records already exist.

--- dev/testScanner.py
+++ dev/testScanner.py
@@ -23,6 +23,7 @@ allMethods = list(methodsFile)
 
 currentClass = ""
 currentPacka = ""
+classUID = -1
 for line in allMethods:
     # New lines are added by the scanner, don't need 'em.
     if line == "\n" or "enum" in line: #Ignore enums and blank lines
@@ -47,12 +48,16 @@ for line in allMethods:
             currentClass = currentClass[0]
 
             #Check the ClassUID table for all records that match the package and class
-            cur.execute("SELECT * FROM testClassUID WHERE testPackage = %s and testClass = %s",(currentPacka, currentClass))
+            cur.execute("SELECT * FROM testClassUID WHERE testPackage = %s and \
+                            testClass = %s",(currentPacka, currentClass))
 
             #If we get any records returned, then obviously it's already in the table we don't
             #have to insert.  Otherwise, if there are no returned records, then we need to
             #insert them into the table.
             if cur.rowcount == 0:
+                #debug
+                print("\nAdding record to DB.         | PKG: " + currentPacka.ljust(20) + " | CLS: " \
+                        + currentClass.ljust(30))
                 try:
                     cur.execute("INSERT INTO testClassUID(testClassUID, testPackage, testClass) \
                                 VALUES (NULL, %s, %s)",(currentPacka, currentClass))
@@ -63,10 +68,24 @@ for line in allMethods:
             else:
                 pass
                 #debug
-                print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(20) + \
-                            " | Already exists in DB.")
+                print("\nRecord already exists in DB. | PKG: " + currentPacka.ljust(20) + " | CLS: " \
+                        + currentClass.ljust(20))
+
+            #Execute the same select, so we can get the new classUID
+            cur.execute("SELECT * FROM testClassUID WHERE testPackage = %s and \
+                            testClass = %s",(currentPacka, currentClass))
+            if cur.rowcount == 0:
+                print("Somehow, we inserted and could not insert a test classUID.  Exiting.")
+                sys.exit()
+            elif cur.rowcount != 1:
+                print("Multiple matches for testclassUID table.  What?")
+                sys.exit()
+            else:
+                #Now we can actually get the number.
+                classUID = int(cur.fetchone()[0])
 
         elif "enum" not in line: #for example: public String getNote () {
+
             #split on the parenthesis, grab the first element. since that's gonna include the
             #method name, and split that on spaces
             part = line.split("(")[0].split(" ")
@@ -89,24 +108,23 @@ for line in allMethods:
             #have to insert.  Otherwise, if there are no returned records, then we need to
             #insert them into the table.
             # TODO: FIX THE UID
-            cur.execute("SELECT * FROM testMethodUID WHERE testClassUID = 0 and \
-                                testMethodName = %s",(test))
+            cur.execute("SELECT * FROM testMethodUID WHERE testClassUID = %d and testMethodName = '%s'" % (classUID, test))
             if cur.rowcount == 0:
                 #debug
-                print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(30) + \
-                        " | MTD: " + test.ljust(40) + " | Adding to DB.")
+                print("Adding record to DB.         | PKG: " + currentPacka.ljust(20) + " | CLS: " \
+                        + currentClass.ljust(30) + " | MTD: " + test.ljust(40))
                 try:
                     cur.execute("INSERT INTO testMethodUID(testMethodUID, testClassUID, testMethodName) VALUES \
-                                    (NULL, 0, %s)",(test))
+                                    (NULL, %s, %s)", (classUID, test))
                 except e:
                     #debug
-                    print(e[0] + "|" + e[1])
+                    #print(e[0] + "|" + e[1])
                     connection.rollback()
             else:
                 pass
                 #debug
-                print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(30) + \
-                        " | MTD: " + test.ljust(40) + " | Already exists in DB.")
+                print("Record already exists in DB. | PKG: " + currentPacka.ljust(20) + " | CLS: " \
+                        + currentClass.ljust(30) + " | MTD: " + test.ljust(40))
 
 
 methodsFile.close()
