2ec6c3da14789051fda033c91cdba4dcf73eb64c
spschoen <spschoen@ncsu.edu>
2017-02-21 18:36:50 -0500

    methodScanner now verifies input better.

--- dev/methodScanner.py
+++ dev/methodScanner.py
@@ -21,33 +21,34 @@ cur = connection.cursor()
 methodsFile = open("methods.txt", "r" )
 allMethods = list(methodsFile)
 
-currentClass = ""
-currentPacka = ""
+newClass = ""
+Pacakge = ""
+classUID = -1
 for line in allMethods:
     # New lines are added by the scanner, don't need 'em.
     if line == "\n" or "enum" in line: #Ignore enums and blank lines
         continue
     else:
         if "dir" in line: #for example: dir bug_tracker
-            currentPacka = line.split(" ")[1].replace("\n","")
+            Pacakge = line.split(" ")[1].replace("\n","")
             #Split the string on spaces, then take the second value
             #which is the directory/package, then remove the new line
 
         elif "class" in line or "interface" in line: #for example: public class TrackedBug {
-            currentClass = line.replace("\n","").split(" ")
+            newClass = line.replace("\n","").split(" ")
             #Remove new line, split on space.
 
             #While we haven't hit class/interface, remove previous elements.
             #Since access can be optional (none is accepted), we have to iterate until we hit
             #class/interface.  Once we get it, we delete class/interface and the first element
             #is the class name.
-            while currentClass[0] != "class" and currentClass[0] != "interface":
-                del currentClass[0]
-            del currentClass[0]
-            currentClass = currentClass[0]
+            while newClass[0] != "class" and newClass[0] != "interface":
+                del newClass[0]
+            del newClass[0]
+            newClass = newClass[0]
 
             #Check the ClassUID table for all records that match the package and class
-            cur.execute("SELECT * FROM classUID WHERE Package = %s and class = %s",(currentPacka, currentClass))
+            cur.execute("SELECT * FROM classUID WHERE Package = %s and class = %s",(Pacakge, newClass))
 
             #If we get any records returned, then obviously it's already in the table we don't
             #have to insert.  Otherwise, if there are no returned records, then we need to
@@ -55,7 +56,7 @@ for line in allMethods:
             if cur.rowcount == 0:
                 try:
                     cur.execute("INSERT INTO classUID(classUID, Package, Class) VALUES \
-                                    (NULL, %s, %s)",(currentPacka, currentClass))
+                                    (NULL, %s, %s)",(Pacakge, newClass))
                 except e:
                     #debug
                     #print(e[0] + "|" + e[1])
@@ -63,8 +64,23 @@ for line in allMethods:
             else:
                 pass
                 #debug
-                #print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(20) + \
+                #print("PKG: " + Pacakge.ljust(20) + " | CLS: " + newClass.ljust(20) + \
                 #            " | Already exists in DB.")
+
+            #Execute the same select, so we can get the new classUID
+            cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s", \
+                            (Pacakge, newClass))
+
+            #Checking again, looking to make sure that we uploaded.
+            if cur.rowcount == 0:
+                print("Somehow, we inserted and could not insert a classUID.  Exiting.")
+                sys.exit()
+            elif cur.rowcount != 1:
+                print("Multiple matches for classUID table.  What?")
+                sys.exit()
+            else:
+                #Now we can actually get the number.
+                classUID = int(cur.fetchone()[0])
 
         elif "enum" not in line: #for example: public String getNote () {
             #split on the parenthesis, grab the first element. since that's gonna include the
@@ -79,27 +95,25 @@ for line in allMethods:
                 if item == "":
                     continue
                 else:
-                    part = item
+                    methodName = item
                     break
 
             #Ignore new lines, for safety.
-            if item == "\n":
+            if methodName == "\n":
                 continue
 
             #If we get any records returned, then obviously it's already in the table we don't
             #have to insert.  Otherwise, if there are no returned records, then we need to
             #insert them into the table.
-            # TODO: FIX NUMBER
-            cur.execute("SELECT * FROM methodUID WHERE ClassUID = 0 and \
-                                Method = %s",(item))
+            cur.execute("SELECT * FROM methodUID WHERE ClassUID = %s and Method = %s", \
+                            (classUID, methodName))
             if cur.rowcount == 0:
                 #debug
-                #print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(30) + \
-                #        " | MTD: " + item.ljust(40) + " | Adding to DB.")
+                #print("PKG: " + Pacakge.ljust(20) + " | CLS: " + newClass.ljust(30) + \
+                #        " | MTD: " + methodName.ljust(40) + " | Adding to DB.")
                 try:
-                    #TODO: FIX NUMBER
                     cur.execute("INSERT INTO methodUID(methodUID, ClassUID, Method) VALUES \
-                                    (NULL, 0, %s)",(item))
+                                    (NULL, %s, %s)",(classUID, methodName))
                 except e:
                     #debug
                     print(e[0] + "|" + e[1])
@@ -107,8 +121,8 @@ for line in allMethods:
             else:
                 pass
                 #debug
-                #print("PKG: " + currentPacka.ljust(20) + " | CLS: " + currentClass.ljust(30) + \
-                #        " | MTD: " + item.ljust(40) + " | Already exists in DB.")
+                #print("PKG: " + Pacakge.ljust(20) + " | CLS: " + newClass.ljust(30) + \
+                #        " | MTD: " + methodName.ljust(40) + " | Already exists in DB.")
 
 
 methodsFile.close()
