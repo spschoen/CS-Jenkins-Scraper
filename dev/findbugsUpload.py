46fc233613010dc5ca22cd0348cdc7cd0c4fd1ab
spschoen <spschoen@ncsu.edu>
2017-02-24 15:47:14 -0500

    Fixing multi-bug findBugs

--- dev/findbugsUpload.py
+++ dev/findbugsUpload.py
@@ -79,59 +79,68 @@ for node in root.childNodes:
                 if classNode.nodeName == "SourceLine":
                     if classNode.hasAttribute("start"):
                         line = int(classNode.getAttribute("start"))
+        print("PKG: " + package)
+        print("CLN: " + className)
+        print("MTD: " + method)
+        print("BGT: " + bugType)
+        print("PRI: " + str(priority))
+        print("RNK: " + str(rank))
+        print("CAT: " + cat)
+        print("LIN: " + str(line))
+        print("------------------")
 
-                # If we get any records returned, then it's already in the table. Otherwise, if there are no returned records,
-                # then we need to insert them into the table.
-                cur.execute("SELECT * FROM methodUID WHERE Method = %s", (method))
+        # If we get any records returned, then it's already in the table. Otherwise, if there are no returned records,
+        # then we need to insert them into the table.
+        cur.execute("SELECT * FROM methodUID WHERE Method = %s", (method))
 
-                if cur.rowcount == 0: #The values do not exist in the testMethodUID table
-                    #Check if classUID values at least exist, which may not be possible if methodUID doesn't exist, but it's double checking.                cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s",(package, className))
-                    cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s", (package, className))
+        if cur.rowcount == 0: #The values do not exist in the testMethodUID table
+                #Check if classUID values at least exist, which may not be possible if methodUID doesn't exist, but it's double checking.
+                cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s", (package, className))
 
-                    if cur.rowcount == 0: #They don't exist, so add the values into the testMethodUID and testClassUID tables
-                    #Insert into classUID table first to generate the classUID for the testMethodUID table
-                        try:
-                            cur.execute("INSERT INTO classUID(classUID, Package, Class)" \
-                            " VALUES (NULL, %s, %s)", (package, className))
-
-                        except e:
-                            print(e[0] + "|" + e[1])
-                            connection.rollback()
-
-                        #Execute the same select, so we can get the new classUID
-                        cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s", (package, className))
-                
-                    #Checking again, looking to make sure that we uploaded.
-                    if cur.rowcount == 0:
-                        print("Somehow, we inserted and could not insert a classUID.  Exiting.")
-                        sys.exit()
-                    elif cur.rowcount != 1:
-                        print("Multiple matches for classUID table.  How even?")
-                        sys.exit()
-                    else:
-                        #Now we can actually get the number.
-                        classUID = int(cur.fetchone()[0])
-
-                    #Insert into methodUID table
+                if cur.rowcount == 0: #They don't exist, so add the values into the testMethodUID and testClassUID tables
+                #Insert into classUID table first to generate the classUID for the testMethodUID table
                     try:
-                        cur.execute("INSERT INTO methodUID(methodUID, ClassUID, Method)" \
-                        " VALUES (NULL, %s, %s)", (classUID, method))
+                        cur.execute("INSERT INTO classUID(classUID, Package, Class)" \
+                        " VALUES (NULL, %s, %s)", (package, className))
 
                     except e:
                         print(e[0] + "|" + e[1])
                         connection.rollback()
-                    #Reset cursor for below
-                    cur.execute("SELECT * FROM methodUID WHERE Method = %s", (method))
- 
-            #Grab methodUID for below. By now, it should definitely exist
-            methodUID = int(cur.fetchone()[0])
-            # This one goes to findbugs
-            try:
-                add_findbugs = ("INSERT INTO findBugs(CommitsUID, MethodUID, BugType, Priority, Rank, Category, Line) " \
-                      "VALUES ( '%d', '%d', '%s', '%d', '%d', '%s', '%d')" % ( -1, methodUID, bugType, priority, rank, cat, line))
-                cur.execute(add_findbugs)
-            except:
-                print("Messup", sys.exc_info())
-                
+
+                    #Execute the same select, so we can get the new classUID
+                    cur.execute("SELECT * FROM classUID WHERE Package = %s and Class = %s", (package, className))
+
+                #Checking again, looking to make sure that we uploaded.
+                if cur.rowcount == 0:
+                    print("Somehow, we inserted and could not insert a classUID.  Exiting.")
+                    sys.exit()
+                elif cur.rowcount != 1:
+                    print("Multiple matches for classUID table.  How even?")
+                    sys.exit()
+                else:
+                    #Now we can actually get the number.
+                    classUID = int(cur.fetchone()[0])
+
+                #Insert into methodUID table
+                try:
+                    cur.execute("INSERT INTO methodUID(methodUID, ClassUID, Method)" \
+                    " VALUES (NULL, %s, %s)", (classUID, method))
+
+                except e:
+                    print(e[0] + "|" + e[1])
+                    connection.rollback()
+                #Reset cursor for below
+                cur.execute("SELECT * FROM methodUID WHERE Method = %s", (method))
+
+        #Grab methodUID for below. By now, it should definitely exist
+        methodUID = int(cur.fetchone()[0])
+        # This one goes to findbugs
+        try:
+            add_findbugs = ("INSERT INTO findBugs(CommitsUID, MethodUID, BugType, Priority, Rank, Category, Line) " \
+                  "VALUES ( '%d', '%d', '%s', '%d', '%d', '%s', '%d')" % ( -1, methodUID, bugType, priority, rank, cat, line))
+            cur.execute(add_findbugs)
+        except:
+            print("Messup", sys.exc_info())
+
 # Closing connection
 connection.close()
