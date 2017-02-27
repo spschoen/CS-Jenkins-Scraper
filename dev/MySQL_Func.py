e5df734b86e81b324d8cd9693fc4945d98d34313
spschoen <spschoen@ncsu.edu>
2017-02-27 00:19:22 -0500

    Worked on library; coverage upload now works.

--- dev/MySQL_Func.py
+++ dev/MySQL_Func.py
@@ -28,12 +28,61 @@ def getMethodUID(IP, user, pw, DB, method, className, package):
     @return methodUID, the UID of the method in the methodUID table.
     """
     #Connecting to DB
-    connection = pymysql.connect(host=IP, user=user, password=pw, db=DB)
+    try:
+        connection = pymysql.connect(host=IP, user=user, password=pw, db=DB)
+    except:
+        for error in sys.exc_info():
+            print(error)
+        sys.exit()
+
     cur = connection.cursor()
 
-    print("Searching for methodUID")
+    #print("Searching for methodUID")
+    classUID = getClassUID(IP=IP, user=user, pw=pw, DB=DB, className=className, package=package)
 
-    classUID = getClassUID(IP, user, pw, DB, className, package)
+    select = "SELECT * FROM methodUID WHERE ClassUID = %s AND Method = %s"
+    cur.execute(select, (classUID, method))
+
+    #print("Selected " + str(cur.rowcount) + " elements.")
+
+    if cur.rowcount == 0:
+        try:
+            insert = "INSERT INTO methodUID(methodUID, classUID, Method) VALUES (Null, %s, %s)"
+            cur.execute(insert, (classUID, method))
+        except e:
+            ErrorString = e[0] + "\n----------\n"
+            ErrorString += e[1] + "\n----------\n"
+            ErrorString += e[2]
+            fromMail = "spschoen.alerts@gmail.com"
+            to = "spschoen.alerts@gmail.com"
+            subject = "Failure to insert into methodUID!"
+            body = "The following insert statement failed:\n\n"
+            body += "%s\n\n".format(insert)
+            body += "With the following variables (package | class):\n\n"
+            body += "%s | %s".format(classUID, method)
+            body += "The following error message was produced:\n\n%s".format(ErrorString)
+
+            email_text = """\
+            From: %s,
+            To: %s,
+            Subject: %s
+
+            %s
+            """, (fromMail, ", ".join(to), subject, body)
+            try:
+                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                server.ehlo()
+                # TODO: SECURE THE PASSWORD BUT FOR NOW YOU HAVE TO EDIT IT IN.
+                server.login("spschoen.alerts@gmail.com", "PASSWORD")
+                server.sendmail(fromMail, to, email_text)
+                server.close()
+            except:
+                print("Couldn't even send an email, wot")
+                for error in sys.exc_info():
+                    print(error)
+
+    cur.execute(select, (package, className))
+    methodUID = str(cur.fetchone()[0])
 
     connection.close()
     return methodUID
@@ -67,13 +116,14 @@ def getClassUID(IP, user, pw, DB, className, package):
         sys.exit()
 
     cur = connection.cursor()
+    cur.execute("DELETE from classUID where Package=%s and Class=%s", ("String", "lang"))
 
-    print("Searching for classUID")
+    #print("Searching for classUID")
 
     select = "SELECT * FROM classUID WHERE Package = %s AND Class = %s"
     cur.execute(select, (package, className))
 
-    print("Selected " + str(cur.rowcount) + " elements.")
+    #print("Selected " + str(cur.rowcount) + " elements.")
 
     if cur.rowcount == 0:
         try:
@@ -110,47 +160,38 @@ def getClassUID(IP, user, pw, DB, className, package):
                 print("Couldn't even send an email, wot")
                 for error in sys.exc_info():
                     print(error)
-    else:
-        try:
-            insert = "INSERT INTO classUID(classUID, Package, Class) VALUES (Null, %s, %s)"
-        except:
-            ErrorString = str(sys.exc_info()[0]) + "\n----------\n"
-            ErrorString += str(sys.exc_info()[1]) + "\n----------\n"
-            ErrorString += str(sys.exc_info()[2])
 
-            msg = MIMEText("The following insert statement failed:\n\n"
-                            + insert
-                            + "\n\nWith the following variables (package | class):\n\n"
-                            + package + " | " + className
-                            + "\n\nThe following error message was produced:"
-                            + "\n\n" + traceback.format_exc())
-
-            msg["To"] = email.utils.formataddr(("Administrator", "spschoen.alerts@gmail.com"))
-            msg["From"] = email.utils.formataddr(("Alert System", "spschoen.alerts@gmail.com"))
-            msg["Subject"] = "Failure to insert into classUID!"
-
-            server = smtplib.SMTP("smtp.gmail.com", 587)
-            server.ehlo()
-            server.starttls()
-            # TODO: SECURE THE PASSWORD BUT FOR NOW YOU HAVE TO EDIT IT IN.
-            server.login("spschoen.alerts@gmail.com", "PASSWORD")
-            server.set_debuglevel(True) # show communication with the server
-            try:
-                server.sendmail("spschoen.alerts@gmail.com", ["spschoen.alerts@gmail.com"], \
-                                    msg.as_string())
-            finally:
-                server.quit()
-
-
-
-    select = "SELECT * FROM classUID WHERE Package = %s AND Class = %s"
-    classUID = int(cur.fetchone()[0])
+    cur.execute(select, (package, className))
+    classUID = str(cur.fetchone()[0])
 
     connection.close()
     return classUID
 
 def getCommitUID(IP, user, pw, DB, hash, repoID):
-    connection = pymysql.connect(host=IP, user=user, password=pw, db=DB)
+    """
+    getCommitUID will take args to connect to a Database, as well as
+    the Project ID and commit Hash, to get the correct commitUID.
+    Will create a commitUID if it doesn't exist.
+
+    @author Samuel Schoeneberger
+    @version 1.0
+
+    @param IP     - the address of the Database to connect to
+    @param user   - the username to log into the DB.  Probably root.
+    @param pw     - the password of the above username.  c'mon man.
+    @param DB     - the specific database to connect to in MySQL.
+    @param hash   - the commit hash of a git commit. 40 hex chars
+    @param repoID - the repo to look up in the commitUID table.  PW-XYZ
+
+    @return methodUID, the UID of the method in the methodUID table.
+    """
+    #Connecting to DB
+    try:
+        connection = pymysql.connect(host=IP, user=user, password=pw, db=DB)
+    except:
+        for error in sys.exc_info():
+            print(error)
+        sys.exit()
     cur = connection.cursor()
 
     CUID = -1
@@ -161,16 +202,12 @@ def getCommitUID(IP, user, pw, DB, hash, repoID):
             cur.execute("INSERT INTO commitUID(commitUID, Hexsha, Repo) VALUES \
                             (NULL, %s, %s)", (hash, repoID) )
             cur.execute(commitUIDSelect, (hash, repoID) )
-            CUID = cur.fetchone()[0]
+            CUID = str(cur.fetchone()[0])
         except e:
             print(e[0] + "|" + e[1])
             connection.rollback()
     else:
-        CUID = cur.fetchone()[0] #Get the actual UID since it exists
-
-    if CUID == -1:
-        print("Could not get CUID")
-        sys.exit()
+        CUID = str(cur.fetchone()[0]) #Get the actual UID since it exists
 
     connection.close()
     return CUID
--- dev/coverageUpload.py
+++ dev/coverageUpload.py
@@ -1,29 +1,76 @@
-from xml.dom.minidom import parse
-import xml.dom.minidom
+"""
+@authors Samuel Schoeneberger 02/2017
+
+Execution: python3 csvReader.py $WORKSPACE $PROJECT_ID $GIT_COMMIT
+0. commitUpload.py
+1. WORKSPACE  : /path/to/report.csv (DO NOT INCLUDE report.csv)
+2. PROJECT_ID : PW-XYZ
+3. GIT_COMMIT : [40 char commit hash]
+
++-------------+---------+------+-----+---------+-------+
+| Field       | Type    | Null | Key | Default | Extra |
++-------------+---------+------+-----+---------+-------+
+| CommitUID   | int(11) | NO   | PRI | NULL    |       |
+| ClassUID    | int(11) | YES  |     | NULL    |       |
+| Coverage    | int(11) | YES  |     | NULL    |       |
+| Instruction | int(11) | YES  |     | NULL    |       |
+| Branch      | int(11) | YES  |     | NULL    |       |
+| Complexity  | int(11) | YES  |     | NULL    |       |
+| Line        | int(11) | YES  |     | NULL    |       |
++-------------+---------+------+-----+---------+-------+
+"""
+
+import csv
 import sys
 import os
-import pymysql.cursors
+import pymysql
+import MySQL_Func
 
-# Setting up the XML to read
-FILE_DIR = os.path.abspath(os.path.join(os.getcwd()))
-for arg in sys.argv[len(sys.argv) - 1].split("/"):
-    if arg == "":
-        continue
-    FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, arg))
-    #print(FILE_DIR)
+if len(sys.argv) != 4:
+    print("Did not get expected args.")
+    sys.exit()
 
-# Setting up the DB connection
-# TODO: Change this to either enter or the master IP.
-# Future people: change this to your master IP
-# Or wherever your DB is.
-connection = pymysql.connect(host="152.46.20.243",
-                                user="root",
-                                password="",
-                                db="repoinfo")
-cur = cnx.cursor()
-# Connection setup
+connection = pymysql.connect(host="152.46.20.243", user="root", password="", db="repoinfo")
+cur = connection.cursor()
 
-# TODO: Actual code goes here.
+# Getting path to report.csv
+FILE_DIR = "/"
+# Iterate through $WORKSPACE to set up the directory.
+for arg in sys.argv[1].split("/"):
+    if arg != "":
+        FILE_DIR = os.path.abspath(os.path.join(FILE_DIR, arg))
 
-# Closing connection
-cnx.close()
+repoID = sys.argv[2]
+hash = sys.argv[3]
+
+commitUID = MySQL_Func.getCommitUID(
+                IP="152.46.20.243",
+                user="root",
+                pw="",
+                DB="repoinfo",
+                hash=hash,
+                repoID=repoID)
+
+# TODO: take argument
+csvfile = open('site/jacoco/report.csv', newline='')
+report = csv.DictReader(csvfile, delimiter=',')
+for row in report:
+    if "gui" not in row['PACKAGE'].lower() and row['CLASS'][-4:].lower() != "test":
+        classUID = MySQL_Func.getClassUID(
+                        IP="152.46.20.243",
+                        user="root",
+                        pw="",
+                        DB="repoinfo",
+                        className=row['CLASS'].split(".")[-1],
+                        package=row['PACKAGE'].split(".")[-1])
+        #print(row['LINE_MISSED'].ljust(3) + " | " + row['LINE_COVERED'].ljust(3))
+        coverage = int(row['LINE_COVERED']) / (int(row['LINE_MISSED']) + int(row['LINE_COVERED']))
+        print(row['CLASS'].split(".")[-1].ljust(16) + " | " + str(round(coverage * 100)))
+
+        insert = "INSERT INTO coverage(CommitUID, ClassUID, Line) VALUES (%s, %s, %s)"
+        try:
+            cur.execute(insert, (commitUID, classUID, str(round(coverage * 100))))
+        except:
+            #TODO: Email on Failure
+            for error in sys.exc_info():
+                print(error)
