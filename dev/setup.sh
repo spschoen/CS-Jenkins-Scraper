00909ba7e722ab98c90fe3254d4e0be60cee984a
spschoen <spschoen@ncsu.edu>
2017-03-01 10:11:01 -0500

    Adding setup.sh

---
+++ dev/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script NEEDS to be executed with sudo or else it won't do any work.
+
+#Update yum, because our versions are ancient.
+update yum
+
+#Install python3 because that's what the scripts run off.
+yum install python34.x86_64
+
+#Install the "count lines of code" utility for analysis.
+yum install cloc
+
+#Install the two Python Modules we use.
+pip3 install PyMySQL
+pip3 install GitPython
+
+#Making sure GitPython will eventually work.
+echo "PATH=$PATH:/usr/local/git/bin/"
