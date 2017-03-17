84685bc43f007ad702d4f87401e436a67344e7c7
spschoen <spschoen@ncsu.edu>
2017-03-17 12:24:54 -0400

    Changing runscript to use proper variables.

--- dev/runScript.sh
+++ dev/runScript.sh
@@ -5,25 +5,25 @@ echo ""
 
 # Removing these from the directory; hiding them from students.
 # Commented out because it can be done in ANT GUYS C'MON
-#rm -rf $WORKDIR/ts_test
-#rm -rf $WORKDIR/ts_bin
+#rm -rf $WORKSPACE/$PROJECT_NAME/ts_test
+#rm -rf $WORKSPACE/$PROJECT_NAME/ts_bin
 
 echo "Data Miner:"
 
 # TODO: Make this work without logging in.  Probably easier than I think.
 #cd /home/jenkins/scripts/
 #git pull
-#cd $WORKDIR
+#cd $WORKSPACE/$PROJECT_NAME
 
 echo "    [Data Miner] Beginning mining."
-cd $WORKDIR
+cd $WORKSPACE/$PROJECT_NAME
 
 ##################################
 # Getting Project ID for CUID    #
 ##################################
 
 # echo "    [Data Miner] Acquiring ProjectID scanner"
-cp /home/jenkins/scripts/dev/splitter.sh $WORKDIR/splitter.sh
+cp /home/jenkins/scripts/dev/splitter.sh $WORKSPACE/$PROJECT_NAME/splitter.sh
 
 # echo "    [Data Miner] Getting Project ID"
 PROJECT_ID=$(bash splitter.sh $GIT_URL)
@@ -33,20 +33,20 @@ PROJECT_ID=$(bash splitter.sh $GIT_URL)
 ##################################
 
 # echo "    [Data Miner] Pulling library functions."
-cp /home/jenkins/scripts/dev/MySQL_Func.py $WORKDIR/MySQL_Func.py
+cp /home/jenkins/scripts/dev/MySQL_Func.py $WORKSPACE/$PROJECT_NAME/MySQL_Func.py
 
 ##################################
 # Scanning for methods           #
 ##################################
 
 # echo "    [Data Miner] Acquiring method scanner"
-cp /home/jenkins/scripts/dev/methodScan.sh $WORKDIR/methodScan.sh
+cp /home/jenkins/scripts/dev/methodScan.sh $WORKSPACE/$PROJECT_NAME/methodScan.sh
 
 # echo "    [Data Miner] Executing method scanner script"
-sh $WORKDIR/methodScan.sh $WORKDIR/ > $WORKDIR/methods.txt
+sh $WORKSPACE/$PROJECT_NAME/methodScan.sh $WORKSPACE/$PROJECT_NAME/ > $WORKSPACE/$PROJECT_NAME/methods.txt
 
 # echo "    [Data Miner] Acquiring method uploader"
-cp /home/jenkins/scripts/dev/methodScanner.py $WORKDIR/methodScanner.py
+cp /home/jenkins/scripts/dev/methodScanner.py $WORKSPACE/$PROJECT_NAME/methodScanner.py
 
 # echo "    [Data Miner] Executing method uploader"
 python3 methodScanner.py
@@ -56,13 +56,13 @@ python3 methodScanner.py
 ##################################
 
 # echo "    [Data Miner] Acquiring test scanner"
-cp /home/jenkins/scripts/dev/testScan.sh $WORKDIR/testScan.sh
+cp /home/jenkins/scripts/dev/testScan.sh $WORKSPACE/$PROJECT_NAME/testScan.sh
 
 # echo "    [Data Miner] Executing test scanner script"
-sh $WORKDIR/testScan.sh $WORKDIR/ > $WORKDIR/tests.txt
+sh $WORKSPACE/$PROJECT_NAME/testScan.sh $WORKSPACE/$PROJECT_NAME/ > $WORKSPACE/$PROJECT_NAME/tests.txt
 
 # echo "    [Data Miner] Acquiring test uploader"
-cp /home/jenkins/scripts/dev/testScanner.py $WORKDIR/testScanner.py
+cp /home/jenkins/scripts/dev/testScanner.py $WORKSPACE/$PROJECT_NAME/testScanner.py
 
 # echo "    [Data Miner] Executing test uploader"
 python3 testScanner.py
@@ -72,7 +72,7 @@ python3 testScanner.py
 ##################################
 
 # echo "    [Data Miner] Acquiring commit information uploader"
-cp /home/jenkins/scripts/dev/commitUpload.py $WORKDIR/commitUpload.py
+cp /home/jenkins/scripts/dev/commitUpload.py $WORKSPACE/$PROJECT_NAME/commitUpload.py
 
 # echo "    [Data Miner] Executing commit information uploader"
 python3 commitUpload.py $WORKSPACE $PROJECT_ID $GIT_COMMIT $BUILD_NUMBER
@@ -82,7 +82,7 @@ python3 commitUpload.py $WORKSPACE $PROJECT_ID $GIT_COMMIT $BUILD_NUMBER
 ##################################
 
 # echo "    [Data Miner] Acquiring checkstyle uploader"
-cp /home/jenkins/scripts/dev/checkstyleUpload.py $WORKDIR/checkstyleUpload.py
+cp /home/jenkins/scripts/dev/checkstyleUpload.py $WORKSPACE/$PROJECT_NAME/checkstyleUpload.py
 
 # echo "    [Data Miner] Executing checkstyle uploader"
 python3 checkstyleUpload.py ./ $PROJECT_ID $GIT_COMMIT
@@ -92,7 +92,7 @@ python3 checkstyleUpload.py ./ $PROJECT_ID $GIT_COMMIT
 ##################################
 
 # echo "    [Data Miner] Acquiring FindBugs uploader"
-cp /home/jenkins/scripts/dev/findbugsUpload.py $WORKDIR/findbugsUpload.py
+cp /home/jenkins/scripts/dev/findbugsUpload.py $WORKSPACE/$PROJECT_NAME/findbugsUpload.py
 
 # echo "    [Data Miner] Executing FindBugs uploader"
 python3 findbugsUpload.py ./ $PROJECT_ID $GIT_COMMIT
@@ -102,7 +102,7 @@ python3 findbugsUpload.py ./ $PROJECT_ID $GIT_COMMIT
 ##################################
 
 # echo "    [Data Miner] Acquiring PMD uploader"
-cp /home/jenkins/scripts/dev/pmdUpload.py $WORKDIR/pmdUpload.py
+cp /home/jenkins/scripts/dev/pmdUpload.py $WORKSPACE/$PROJECT_NAME/pmdUpload.py
 
 # echo "    [Data Miner] Executing PMD uploader"
 python3 pmdUpload.py ./ $PROJECT_ID $GIT_COMMIT
@@ -112,7 +112,7 @@ python3 pmdUpload.py ./ $PROJECT_ID $GIT_COMMIT
 ##################################
 
 # echo "    [Data Miner] Acquiring Test Results uploader"
-cp /home/jenkins/scripts/dev/testFileResultsUpload.py $WORKDIR/testFileResultsUpload.py
+cp /home/jenkins/scripts/dev/testFileResultsUpload.py $WORKSPACE/$PROJECT_NAME/testFileResultsUpload.py
 
 # echo "    [Data Miner] Executing Test Results uploader"
 python3 testFileResultsUpload.py ./ $PROJECT_ID $GIT_COMMIT
@@ -122,10 +122,10 @@ python3 testFileResultsUpload.py ./ $PROJECT_ID $GIT_COMMIT
 ##################################
 
 # echo "    [Data Miner] Acquiring Coverage uploader"
-cp /home/jenkins/scripts/dev/coverageUpload.py $WORKDIR/coverageUpload.py
+cp /home/jenkins/scripts/dev/coverageUpload.py $WORKSPACE/$PROJECT_NAME/coverageUpload.py
 
 # echo "    [Data Miner] Executing Coverage uploader"
-python3 coverageUpload.py $WORKDIR $PROJECT_ID $GIT_COMMIT
+python3 coverageUpload.py $WORKSPACE/$PROJECT_NAME $PROJECT_ID $GIT_COMMIT
 
 ##################################
 # Cleaning up the local dir.     #
