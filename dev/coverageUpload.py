9e744e7ad7dc6ac950fbfcc47876235968782135
spschoen <spschoen@ncsu.edu>
2017-05-01 21:03:47 -0400

    CoverageUpload uploads ALL Coverage.

--- dev/coverageUpload.py
+++ dev/coverageUpload.py
@@ -49,36 +49,55 @@ if not os.path.exists(FILE_DIR + '/report.csv'):
     sys.exit()
 
 try:
-    csvfile = open(FILE_DIR + "/report.csv", newline='')
+    csv_file = open(FILE_DIR + "/report.csv", newline='')
 except:
     print("Failed to open report.csv file; please contact a TA.")
     sys.exit()
 
 
-report = csv.DictReader(csvfile, delimiter=',')
+report = csv.DictReader(csv_file, delimiter=',')
 for row in report:
-    if "gui" not in row['PACKAGE'].lower() and row['CLASS'][-4:].lower() != "test":
-        class_uid = Scraper.get_class_uid(ip=config_info['ip'], user=config_info['user'], pw=config_info['pass'],
-                                          db=config_info['db'], class_name=row['CLASS'].split(".")[-1],
-                                          package=row['PACKAGE'].split(".")[-1])
+    if "gui" in row['PACKAGE'].lower() or row['CLASS'][-4:].lower() == "test":
+        continue
 
-        coverage = int(row['LINE_COVERED']) / (int(row['LINE_MISSED']) + int(row['LINE_COVERED']))
-        coverage = str(round(coverage * 100))
+    class_uid = Scraper.get_class_uid(ip=config_info['ip'], user=config_info['user'], pw=config_info['pass'],
+                                      db=config_info['db'], class_name=row['CLASS'].split(".")[-1],
+                                      package=row['PACKAGE'].split(".")[-1])
 
-        search = "SELECT * FROM coverage WHERE CommitUID = %s AND ClassUID = %s AND Line_Coverage = %s"
-        cur.execute(search, (commit_uid, class_uid, coverage))
-        if cur.rowcount != 0:
-            continue
+    coverage = int(row['LINE_COVERED']) / (int(row['LINE_MISSED']) + int(row['LINE_COVERED']))
+    coverage = str(round(coverage * 100))
 
-        insert = "INSERT INTO coverage(CommitUID, ClassUID, Line_Coverage) VALUES (%s, %s, %s)"
-        try:
-            cur.execute(insert, (commit_uid, class_uid, coverage))
-        except:
-            connection.rollback()
-            ErrorString = sys.exc_info()[0] + "\n----------\n"
-            ErrorString += sys.exc_info()[1] + "\n----------\n"
-            ErrorString += sys.exc_info()[2]
-            Scraper.sendFailEmail("Failed to insert into Coverage table!", "The following insert failed:", insert,
-                                  "(CommitUID, ClassUID, Line)", ErrorString, commit_uid, class_uid, coverage)
+    instruction = int(row['INSTRUCTION_COVERED']) / (int(row['INSTRUCTION_MISSED']) + int(row['INSTRUCTION_COVERED']))
+    instruction = str(round(instruction * 100))
+
+    branch = int(row['BRANCH_COVERED']) / (int(row['BRANCH_MISSED']) + int(row['BRANCH_COVERED']))
+    branch = str(round(branch * 100))
+
+    complexity = int(row['COMPLEXITY_COVERED']) / (int(row['COMPLEXITY_MISSED']) + int(row['COMPLEXITY_COVERED']))
+    complexity = str(round(complexity * 100))
+
+    method = int(row['METHOD_COVERED']) / (int(row['METHOD_MISSED']) + int(row['METHOD_COVERED']))
+    method = str(round(method * 100))
+
+    search = "SELECT * FROM coverage WHERE CommitUID = %s AND ClassUID = %s AND Line_Coverage = %s AND " \
+             "Instruction_Coverage = %s AND Branch_Coverage = %s AND Complexity_Coverage = %s AND Method_Coverage = %s"
+    cur.execute(search, (commit_uid, class_uid, coverage, instruction, branch, complexity, method))
+    if cur.rowcount != 0:
+        continue
+
+    insert = "INSERT INTO coverage(CommitUID, ClassUID, Line_Coverage, Instruction_Coverage, Branch_Coverage," \
+             "Complexity_Coverage, Method_Coverage) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    try:
+        cur.execute(insert, (commit_uid, class_uid, coverage, instruction, branch, complexity, method))
+    except:
+        connection.rollback()
+        ErrorString = sys.exc_info()[0] + "\n----------\n"
+        ErrorString += sys.exc_info()[1] + "\n----------\n"
+        ErrorString += sys.exc_info()[2]
+        fail_list = "(CommitUID, ClassUID, Line_Coverage, Instruction_Coverage, Branch_Coverage, "
+        fail_list += "Complexity Coverage, Method_Coverage)"
+        Scraper.sendFailEmail("Failed to insert into Coverage table!", "The following insert failed:", insert,
+                              fail_list, ErrorString, commit_uid, class_uid,
+                              (commit_uid, class_uid, coverage, instruction, branch, complexity, method))
 
 connection.close()
